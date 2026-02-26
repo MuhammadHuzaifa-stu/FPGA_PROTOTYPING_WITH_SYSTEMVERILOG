30450d57b6098008f87acf889263fc9c78e7b32c
Muhammad Huzaifa <120576662+MuhammadHuzaifa-stu@users.noreply.github.com>
2026-02-26 15:38:16 +0500

    optimized fifo_ctrl

--- Memory_Design_Examples/FIFO_with_Status/src/fifo_ctrl.sv
+++ Memory_Design_Examples/FIFO_with_Status/src/fifo_ctrl.sv
@@ -34,28 +34,18 @@ module fifo_ctrl #(
     logic [ADDR_WIDTH  :0] counter;
     logic [ADDR_WIDTH  :0] counter_next;
 
-    logic                  full_logic;
-    logic                  full_next;
-    
-    logic                  empty_logic;
-    logic                  empty_next;
-
     always_ff @( posedge clk or negedge arst_n ) 
     begin
         if (!arst_n)
         begin
             wr_ptr      <= 'd0;
             rd_ptr      <= 'd0;
-            full_logic  <= 'd0;
-            empty_logic <= 'd1;
             counter     <= 'd0;
         end
         else
         begin
             wr_ptr      <= wr_ptr_next;
             rd_ptr      <= rd_ptr_next;
-            full_logic  <= full_next;
-            empty_logic <= empty_next;
             counter     <= counter_next;
         end
     end
@@ -68,31 +58,19 @@ module fifo_ctrl #(
         counter_next = counter;
         wr_ptr_next  = wr_ptr;
         rd_ptr_next  = rd_ptr;
-        full_next    = full_logic;
-        empty_next   = empty_logic;
         case ({wr_en, rd_en})
             2'b01 :begin //read
-                if (!empty_logic)
+                if (!empty)
                 begin
                     rd_ptr_next   = rd_ptr_succ;
                     counter_next  = counter - 'd1;
-                    full_next     = 'd0;
-                    if (rd_ptr_succ == wr_ptr)
-                    begin
-                        empty_next = 'd1;
-                    end
                 end
             end 
             2'b10 :begin //write
-                if (!full_logic)
+                if (!full)
                 begin
                     wr_ptr_next  = wr_ptr_succ;
                     counter_next = counter + 'd1;
-                    empty_next   = 'd0;
-                    if (wr_ptr_succ == rd_ptr)
-                    begin
-                        full_next = 'd1;
-                    end
                 end
             end 
             2'b11 :begin //write and read
@@ -105,8 +83,8 @@ module fifo_ctrl #(
 
     assign wr_addr      = wr_ptr;
     assign rd_addr      = rd_ptr;
-    assign full         = full_logic;
-    assign empty        = empty_logic;
+    assign full         = counter == DEPTH;
+    assign empty        = counter == 'd0;
     assign almost_full  = counter >= AF_LVL;
     assign almost_empty = counter <= AE_LVL;
     assign w_count      = counter;
