3e98d051f1ed37fe6202ed6faa6ff6223e303762
Muhammad Huzaifa <120576662+MuhammadHuzaifa-stu@users.noreply.github.com>
2026-03-02 11:06:02 +0500

    added timer_core

---
+++ Microblaze/hardware/src/chu_timer.sv
@@ -0,0 +1,58 @@
+module chu_timer #(
+    localparam ADDR_WIDTH    = 5,
+    localparam DATA_WIDTH    = 32,
+    localparam COUNTER_WIDTH = 48
+) (
+    input  logic                  clk, 
+    input  logic                  arst_n,
+    // ctrl
+    input  logic                  cs,
+    input  logic                  wr_en,
+    input  logic                  rd_en,
+    // data
+    input  logic [ADDR_WIDTH-1:0] addr,
+    input  logic [DATA_WIDTH-1:0] wdata,
+    output logic [DATA_WIDTH-1:0] rdata,
+);
+
+    logic [COUNTER_WIDTH-1:0] counter_reg;
+    logic                     ctrl_reg;
+    logic                     wr;
+    logic                     clr;
+    logic                     go;
+
+    always_ff @( posedge clk or negedge arst_n ) 
+    begin : counter_blk
+        if (!arst_n)
+        begin
+            counter_reg <= '0;
+        end
+        else if (clr)
+        begin
+            counter_reg <= '0;
+        end
+        else if (go)
+        begin
+            counter_reg <= counter_reg + 1;
+        end
+    end
+
+    always_ff @( posedge clk or negedge arst_n ) 
+    begin : ctrl_reg_blk
+        if (!arst_n)
+        begin
+            ctrl_reg <= '0;
+        end
+        else if (wr)
+        begin
+            ctrl_reg <= wdata[0];
+        end
+    end
+
+    assign wr  = wr_en && cs && (addr[1:0] == 2'b10);
+    assign clr = wr && wdata[1];
+    assign go  = ctrl_reg;
+
+    assign rdata = (addr[0]) ? {(COUNTER_WIDTH-32){1'b0}, counter_reg[COUNTER_WIDTH-1:32]} : counter_reg[31:0];
+
+endmodule
