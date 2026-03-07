4bd4d0a6bbb4346085779e42585685bfea271cd0
Muhammad Huzaifa <120576662+MuhammadHuzaifa-stu@users.noreply.github.com>
2026-03-07 21:39:43 +0500

    added display functions in uartcore

--- Microblaze/software/src/uart_core.cpp
+++ Microblaze/software/src/uart_core.cpp
@@ -46,3 +46,106 @@ int UartCore::rx_byte() {
         return (int) data;
     }
 }
+
+// DISPLAY Methods
+void UartCore::disp_str(const char* str) {
+    while ((uint8_t) *str) {
+        tx_byte((uint8_t) *str);
+        str++;
+    }
+}
+
+void UartCore::disp(char ch) {
+    tx_byte((uint8_t) ch);
+}
+
+void UartCore::disp(const char* str) {
+    disp_str(str);
+}
+
+void UartCore::disp(int n, int base, int len) {
+    char buf[33]; // enough for 32 bits + null terminator
+    char *str, ch, sign;
+    int i, rem;
+    unsigned int un;
+
+    // error check
+    if (base != 2 && base != 8 && base != 16)
+        base = 10;
+    if (len > 32) // error check
+        len = 32;
+    // handle -ive decimal
+    if (base == 10 && n < 0) {
+        un = (unsigned) -n;
+        sign = '-';
+    } else {
+        un = (unsigned) n;
+        sign = ' ';
+    }
+    // convert to string
+    i = 0;
+    str = &buf[33];
+    *str = '\0';
+    do {
+        str--;
+        rem = un % base;
+        un = un / base;
+        if (rem < 10)
+            ch = (char) rem + '0';
+        else
+            ch = (char) rem - 10 + 'a';
+        *str = ch;
+        i++;
+    } while (un);
+    // attach - sign for neg decimal number
+    if (sign == '-') {
+        str--;
+        *str = sign;
+        i++;
+    }
+    // pad with blank if needed
+    while (i < len) {
+        str--;
+        *str = ' ';
+        i++;
+    }
+    // display the string
+    disp_str(str);
+}
+
+void UartCore::disp(int n, int base) {
+    disp(n, base, 0);
+}
+
+void UartCore::disp(int n) {
+    disp(n, 10, 0);
+}
+
+void UartCore::disp(double f, int digit) {
+    double fa, frac;
+    int i, n, i_part;
+
+    // obtain abs value of f
+    fa = f;
+    if (f < 0.0) {
+        fa = -f;
+        disp_str("-"); // display - sign for negative number
+    }
+    // obtain integer part
+    i_part = (int) fa;
+    disp(i_part); // display integer part
+    disp_str("."); // display decimal point
+    // obtain fractional part
+    frac = fa - (double) i_part;
+    // display fractional part
+    for (n = 0; n < digit; n++) {
+        frac = frac * 10.0;
+        i = (int) frac;
+        disp(i);
+        frac = frac - i;
+    }
+}
+
+void UartCore::disp(double f) {
+    disp(f, 3);
+}
